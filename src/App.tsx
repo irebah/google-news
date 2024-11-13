888ce5e2c43f1a3c54f6f2ffb951baa867e86677
Ismail Rebah <ismail.rebah@gmail.com>
2024-11-14 10:58:52 +1300

    chore: initialize project
    prepared a project with react, typescript, vite and tailwind

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <span className="text-red-400">ismail</span>;
+};
+
+export default App;
---
+++ tailwind.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {},
+  plugins: [
+    function ({ addBase }: { addBase: (base: Record<string, { [key: string]: string }>) => void }) {
+      addBase({
+        "*": {
+          boxSizing: "border-box",
+          userSelect: "none",
+        },
+        "*::before": {
+          boxSizing: "border-box",
+        },
+        "*::after": {
+          boxSizing: "border-box",
+        },
+      });
+    },
+  ],
+};
+
+export default config;
